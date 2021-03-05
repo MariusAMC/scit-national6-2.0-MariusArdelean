2bdb377218e2d478599543fbc16919f7fdd50c9c
MariusAMC <marius20amc@gmail.com>
2021-03-05 17:33:34 +0200

    functionality done

--- digitalClock.js
+++ digitalClock.js
@@ -1,17 +1,25 @@
 console.log("JavaScript - Digital Clock");
 
 let seconds = 0;
-const secondParagraphs = document.querySelectorAll(".seconds p");
-console.log(secondParagraphs);
+const secondParagraph = document.querySelectorAll(".seconds p");
+console.log(secondParagraph);
 
 let minutes = 0;
-const minuteParagraph = document.querySelectorAll(".minutes p")
+const minuteParagraph = document.querySelectorAll(".minutes p");
 
 let hours = 0;
-const hoursParagraph = document.querySelectorAll(".hours p")
+const hoursParagraph = document.querySelectorAll(".hours p");
 
-setInterval(function(){
-    renderDigits(seconds, secondParagraphs);
+const startButton = document.getElementById("start-timer");
+const stopButton = document.getElementById("stop-timer");
+const resetButton = document.getElementById("reset-timer");
+const saveButton = document.getElementById("save-timer");
+
+let time;
+
+startButton.addEventListener("click", function() {
+    time = setInterval(function (){
+    renderDigits(seconds, secondParagraph);
     renderDigits(minutes, minuteParagraph);
     renderDigits(hours, hoursParagraph);
 
@@ -29,8 +37,32 @@ setInterval(function(){
     if (hours ===24) {
         hours = 0;
     }
+})}, 100);
 
-}, 1);
+stopButton.addEventListener("click", function () {
+    clearInterval(time);
+});
+
+resetButton.addEventListener("click",function() {
+    clearInterval(time);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    resetTime(secondParagraph);
+    resetTime(minuteParagraph);
+    resetTime(hoursParagraph);
+    
+});
+
+saveButton.addEventListener("click", function () {
+    const savedTime = document.createElement("p");
+    document.body.appendChild(savedTime);
+    let newSec = checkTime(seconds);
+    let newMin = checkTime(minutes);
+    let newHours = checkTime(hours);
+    savedTime.innerText = `${newHours}:${newMin}:${newSec}`;
+})
 
 function renderDigits(nr, plist) {
     const stringDigits = nr + "";
@@ -43,4 +75,17 @@ function renderDigits(nr, plist) {
         plist[0].innerText = 0;
         plist[1].innerText = digitList[0];
 }
+}
+
+function resetTime(pList) {
+    pList[0].innerText = 0;
+    pList[1].innerText = 0;
+}
+
+
+function checkTime(digit) {
+    if (digit < 10) {
+        digit = `0${digit}`; 
+    } 
+    return digit;
 }
