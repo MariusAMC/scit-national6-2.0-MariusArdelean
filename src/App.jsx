cf08c9c093ec699711194941709df8a7fba920db
MariusAMC <marius20amc@gmail.com>
2021-04-27 20:38:30 +0300

    course almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { Header } from "./components/Header/Header";
 import { ToDoList } from "./components/ToDoList/ToDoList";
+import { Component } from "react";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App" id="app">
-      <Header />
-      <ToDoList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    showToDoList: true,
+  };
+
+  toggleToDoListVisibility = () => {
+    this.setState({ showToDoList: !this.state.showToDoList });
+  };
+
+  render() {
+    return (
+      <div className="App" id="app">
+        <Header />
+        <button onClick={this.toggleToDoListVisibility}>
+          Hide/Show ToDoList
+        </button>
+        {this.state.showToDoList ? <ToDoList /> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,14 +1,28 @@
 import "./ToDoItem.css";
 
-export function ToDoItem(props) {
-  // console.log(props);
+// This is a function react component
+// by using props out reusable component can adapt to attributes passed in its parent component
+// to see how this component is used check ToDoList.jsx file
+// we use "{}" to evaluate values and interpolate them in our html objects
+// every classic react component (div, p, input, etc.) use event-like attribute (onClick, onChange, onMouseDown, etc) to link a function to an event
+// in props we can pass anything from strings, bool, number to objects and functions.
+// we pass a "removeItem" method from the parent component so that when user click on trash can we trigger the "removeItem" function that is defined in "ToDoList.jsx"
 
+// for more info in Component and Props: https://reactjs.org/docs/components-and-props.html
+
+export function ToDoItem(props) {
   return (
     <div className="to-do-item">
-      <input type="checkbox" defaultChecked={props.checkValue} />
+      <input
+        type="checkbox"
+        defaultChecked={props.checkValue}
+        onChange={() => {
+          props.updateCheckStatus(props.itemIndex, !props.checkValue);
+        }}
+      />
       <p>{props.label}</p>
       <img
-        src="https://www.flaticon.com/svg/vstatic/svg/748/748023.svg?token=exp=1619450332~hmac=61409cc4673b5a0bd6635c6bf0f81c9b"
+        src="https://image.freepik.com/free-icon/trash-bin-symbol_318-10194.jpg"
         alt="trash"
         onClick={(event) => props.removeItem(props.label)}
       />
