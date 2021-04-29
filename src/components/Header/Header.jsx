d5788db70b54fbdb8c09a3148de4e464fff8a0d8
MariusAMC <marius20amc@gmail.com>
2021-04-29 20:01:56 +0300

    half time

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { AppLogo } from "../AppLogo/AppLogo";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
 
@@ -7,6 +8,10 @@ export function Header() {
     <div className="app-header">
       <AppLogo />
       <p className="app-header__title">To Do App</p>
+      <div className="navigation">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
     </div>
   );
 }
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,9 @@
+export function About() {
+  return (
+    <div className="about">
+      <h2>To Do App info</h2>
+      <p>This app will store your task on server</p>
+      <p>Feel free to add as many as you like on the homepage</p>
+    </div>
+  );
+}
