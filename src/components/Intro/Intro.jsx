3aaee2c01ac77abe8184313a8e0bca98a249905d
MariusAMC <marius20amc@gmail.com>
2021-05-16 23:21:14 +0300

    Sunday night=go to sleep

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,12 @@
 import "./Intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer"></div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 }
