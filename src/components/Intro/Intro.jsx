a1865829ec242a777abbc8104b0a07be4b0c2fbb
MariusAMC <marius20amc@gmail.com>
2021-05-16 16:52:01 +0300

    created files and folders - ready to blow

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,5 @@
+import "./Intro.css";
+
+export default function Intro() {
+  return <div className="intro">Intro</div>;
+}
---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,5 @@
+import "./Work.css";
+
+export default function Work() {
+  return <div className="work">Work</div>;
+}
