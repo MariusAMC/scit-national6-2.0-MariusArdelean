1f062b582ce562c8a99de753e8a43329f0b5a41a
MariusAMC <marius20amc@gmail.com>
2021-05-17 21:10:25 +0300

    starting to add in portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,52 @@
 import "./Portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://lh3.googleusercontent.com/proxy/PjFZN83ILpqbMrxpHtT8h2T70POlcIBRmViwPng2J8msZIyww6Jc2XmFMXxeYFAn73ARp557g_MGXv78cPIiYTrzQkdxmUC2paD8ik04wzgdMopOE6365064haAv"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://lh3.googleusercontent.com/proxy/PjFZN83ILpqbMrxpHtT8h2T70POlcIBRmViwPng2J8msZIyww6Jc2XmFMXxeYFAn73ARp557g_MGXv78cPIiYTrzQkdxmUC2paD8ik04wzgdMopOE6365064haAv"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://lh3.googleusercontent.com/proxy/PjFZN83ILpqbMrxpHtT8h2T70POlcIBRmViwPng2J8msZIyww6Jc2XmFMXxeYFAn73ARp557g_MGXv78cPIiYTrzQkdxmUC2paD8ik04wzgdMopOE6365064haAv"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://lh3.googleusercontent.com/proxy/PjFZN83ILpqbMrxpHtT8h2T70POlcIBRmViwPng2J8msZIyww6Jc2XmFMXxeYFAn73ARp557g_MGXv78cPIiYTrzQkdxmUC2paD8ik04wzgdMopOE6365064haAv"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://lh3.googleusercontent.com/proxy/PjFZN83ILpqbMrxpHtT8h2T70POlcIBRmViwPng2J8msZIyww6Jc2XmFMXxeYFAn73ARp557g_MGXv78cPIiYTrzQkdxmUC2paD8ik04wzgdMopOE6365064haAv"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
