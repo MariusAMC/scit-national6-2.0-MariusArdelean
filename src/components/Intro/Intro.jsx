0e1ea54f029809b142b2459e8f2c1974d1275cbd
MariusAMC <marius20amc@gmail.com>
2021-05-17 18:50:18 +0300

    added assets folder and files - down arrow created

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,9 +4,22 @@ export default function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="left">
-        <div className="imgContainer"></div>
+        <div className="imgContainer">
+          <img src="assets/me.jpg" alt="" />
+        </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hey there! I'm</h2>
+          <h1>Marius Ardelean</h1>
+          <h3>
+            Web developer <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
