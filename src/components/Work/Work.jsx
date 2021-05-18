7ca668514be796762068c13c15dc0c643ab5abf1
MariusAMC <marius20amc@gmail.com>
2021-05-18 17:54:53 +0300

    add slider function and props/states. next animate

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./Work.scss";
 
 export default function Work() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [
     {
       id: "1",
@@ -25,9 +28,18 @@ export default function Work() {
     },
   ];
 
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
   return (
     <div className="work" id="work">
-      <div className="slider">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((d) => (
           <div className="container">
             <div className="item">
@@ -51,8 +63,18 @@ export default function Work() {
           </div>
         ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
     </div>
   );
 }
