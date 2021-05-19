92a26d23e2e1e8065b9d1e738e4afedd09ead990
MariusAMC <marius20amc@gmail.com>
2021-05-19 16:51:21 +0300

    worked a little for About.jsx-still working

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,43 @@
 import "./About.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function About() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["test", "testulet", "mai"],
+    });
+  }, []);
+
   return (
     <div className="about" id="about">
-      <div className="about-text">
+      <div className="left">
+        <div className="img">
+          <img src="/assets/me.jpg" alt="" />
+        </div>
+      </div>
+      <div className="right">
         <h1>
           About <span className="me">Me</span>
           <span className="underline"></span>
         </h1>
-      </div>
-      <div className="img-and-text">
-        <div className="img">
-          <img src="/assets/me.jpg" alt="" />
+        <div className="text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit odit,
+            magnam nobis amet quisquam, a voluptates harum porro eligendi aut
+            voluptate provident alias quas officia reiciendis! Nostrum maxime
+            dolor sequi.
+          </p>
+          <h3>
+            <span ref={textRef}></span>
+          </h3>
         </div>
-        <div className="text"></div>
+        <button className="dld-button">Download CV</button>
       </div>
     </div>
   );
