ef9e0e6bc6b433678b131c0eeb56cc8b5d9f9e1a
MariusAMC <marius20amc@gmail.com>
2021-05-23 21:19:05 +0300

    Done for Sunday- inserted animations-hobby not

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import "./About.scss";
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
+import Flash from "react-reveal/Flash";
+import Fade from "react-reveal/Fade";
 
 export default function About() {
   const textRef = useRef();
@@ -10,34 +12,49 @@ export default function About() {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["test", "testulet", "mai"],
+      strings: [
+        "Web Developer",
+        "Dancer",
+        "Traveler",
+        "Gamer",
+        "Sci-Fi Fan",
+        "Laptops repair-man",
+      ],
     });
   }, []);
 
   return (
     <div className="about" id="about">
       <div className="left">
-        <div className="img">
-          <img src="/assets/me.jpg" alt="" />
-        </div>
+        <Fade left>
+          <div className="img">
+            <img src="/assets/me.jpg" alt="" />
+          </div>
+        </Fade>
       </div>
       <div className="right">
         <h1>
           About <span className="me">Me</span>
           <span className="underline"></span>
         </h1>
-        <div className="text">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit odit,
-            magnam nobis amet quisquam, a voluptates harum porro eligendi aut
-            voluptate provident alias quas officia reiciendis! Nostrum maxime
-            dolor sequi.
-          </p>
-          <h3>
+        <Fade right>
+          <div className="text">
+            <h3 className="hi">Hi! I'm Marius and I am a</h3>
             <span ref={textRef}></span>
-          </h3>
-        </div>
-        <button className="dld-button">Download CV</button>
+            <p>
+              Ambitious, hardworking and especially very curious. I started
+              learning about Web development 4 months ago, and now here I am
+              making the best I can do. I can't wait to discover more and more
+              about this "TECH" world that I stepped in to. Working on this
+              project made me learn lots of things every day and every hour of
+              the day. My goal is to become a full-time Web Developer, gather a
+              lot of experience and then climb the latter of SUCCESS
+            </p>
+          </div>
+        </Fade>
+        <Flash>
+          <button className="dld-button">Download CV</button>
+        </Flash>
       </div>
     </div>
   );
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,84 +2,107 @@ import PhoneIcon from "@material-ui/icons/Phone";
 import EmailIcon from "@material-ui/icons/Email";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import SendIcon from "@material-ui/icons/Send";
+import Rotate from "react-reveal/Rotate";
+import Slide from "react-reveal/Slide";
+import Fade from "react-reveal/Fade";
 import "./Contact.scss";
 
 export default function Contact() {
   return (
     <div className="contact section" id="contact">
       <div className="title-only">
-        <h1>
-          Contact <span className="me">Me</span>
-        </h1>
-        <span className="underline"></span>
-        <span className="subtitle">Get in touch</span>
+        <Fade down>
+          <h1>
+            Contact <span className="me">Me</span>
+          </h1>
+        </Fade>
+        <Slide left>
+          <span className="underline"></span>
+        </Slide>
+        <Fade up>
+          <span className="subtitle">Get in touch</span>
+        </Fade>
       </div>
       <div className="sides">
         <div className="left">
           <div className="container">
             <div>
-              <div className="info">
-                <PhoneIcon className="phone-icon" />
-                <div>
-                  <h3 className="contact-title">Call Me</h3>
-                  <span className="contact-subtitle">+40 746 929 362</span>
+              <Rotate top left>
+                <div className="info">
+                  <PhoneIcon className="phone-icon" />
+                  <div>
+                    <h3 className="contact-title">Call Me</h3>
+                    <span className="contact-subtitle">+40 746 929 362</span>
+                  </div>
                 </div>
-              </div>
-              <div className="info">
-                <EmailIcon className="phone-icon" />
-                <div>
-                  <h3 className="contact-title">Email</h3>
-                  <span className="contact-subtitle">
-                    marius20amc@gmail.com
-                  </span>
+              </Rotate>
+              <Slide left>
+                <div className="info">
+                  <EmailIcon className="phone-icon" />
+                  <div>
+                    <h3 className="contact-title">Email</h3>
+                    <span className="contact-subtitle">
+                      marius20amc@gmail.com
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="info">
-                <LocationOnIcon className="phone-icon" />
-                <div>
-                  <h3 className="contact-title">Location</h3>
-                  <span className="contact-subtitle">
-                    Baia Mare - Maramures
-                  </span>
+              </Slide>
+              <Rotate bottom left>
+                <div className="info">
+                  <LocationOnIcon className="phone-icon" />
+                  <div>
+                    <h3 className="contact-title">Location</h3>
+                    <span className="contact-subtitle">
+                      Baia Mare - Maramures
+                    </span>
+                  </div>
                 </div>
-              </div>
+              </Rotate>
             </div>
           </div>
         </div>
         <div className="right">
           <form action="" className="contact-form-grid">
             <div className="contact-inputs">
+              <Fade right>
+                <div className="contact-content">
+                  <label htmlFor="" className="contact-label">
+                    Name
+                  </label>
+                  <input type="text" className="input" />
+                </div>
+              </Fade>
+              <Fade right>
+                <div className="contact-content">
+                  <label htmlFor="" className="contact-label">
+                    Email
+                  </label>
+                  <input type="email" className="input" />
+                </div>
+              </Fade>
+            </div>
+            <Fade right>
               <div className="contact-content">
                 <label htmlFor="" className="contact-label">
-                  Name
+                  Message
                 </label>
-                <input type="text" className="input" />
+                <textarea
+                  name=""
+                  id="text"
+                  cols="0"
+                  rows="7"
+                  className="input"
+                ></textarea>
               </div>
-              <div className="contact-content">
-                <label htmlFor="" className="contact-label">
-                  Email
-                </label>
-                <input type="email" className="input" />
+            </Fade>
+            <Fade up>
+              <div className="btn">
+                <button className="button">
+                  <span>Send Message</span>
+                  <SendIcon />
+                </button>
               </div>
-            </div>
-            <div className="contact-content">
-              <label htmlFor="" className="contact-label">
-                Message
-              </label>
-              <textarea
-                name=""
-                id="text"
-                cols="0"
-                rows="7"
-                className="input"
-              ></textarea>
-            </div>
-            <div className="btn">
-              <button className="button">
-                <span>Send Message</span>
-                <SendIcon />
-              </button>
-            </div>
+            </Fade>
           </form>
         </div>
       </div>
