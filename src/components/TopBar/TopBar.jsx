281679fd96d43f324265e5297ae064180d889dbf
MariusAMC <marius20amc@gmail.com>
2021-05-26 23:37:06 +0300

    small things remaining

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,3 +1,9 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+  FaLinkedinIn,
+} from "react-icons/fa";
 import "./TopBar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
@@ -16,6 +22,40 @@ export default function TopBar({ menuOpen, setMenuOpen }) {
           <div className="item-container">
             <Mail className="icon" />
             <span className="mail-address">marius20amc@gmail.com</span>
+          </div>
+          <div class="wrapper">
+            <div class="button">
+              <div class="icon">
+                <i class="fab fa-facebook-f">
+                  <FaFacebook />
+                </i>
+              </div>
+              <span>Facebook</span>
+            </div>
+            <div class="button">
+              <div class="icon">
+                <i class="fab fa-twitter">
+                  <FaInstagram />
+                </i>
+              </div>
+              <span>Instagram</span>
+            </div>
+            <div class="button">
+              <div class="icon">
+                <i class="fab fa-github">
+                  <FaGithub />
+                </i>
+              </div>
+              <span>Github</span>
+            </div>
+            <div class="button">
+              <div class="icon">
+                <i class="fab fa-youtube">
+                  <FaLinkedinIn />
+                </i>
+              </div>
+              <span>LikedIn</span>
+            </div>
           </div>
         </div>
         <div className="right">
